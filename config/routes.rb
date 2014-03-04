26acae37793606ff9823505cdc4cc1a16fa4951b
ola <ola.seb@hotmail.fr>
2014-03-04 12:18:33 +0100

    ajout controller Home et modif des vues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 JewelBrain::Application.routes.draw do
+  get "home/index"
+  root 'jewels#index'
   resources :jewels
 
   # The priority is based upon order of creation: first created -> highest priority.
